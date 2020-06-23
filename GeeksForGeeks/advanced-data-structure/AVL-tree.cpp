d3fc4bd2e7e25a8fce8f3f12908fdbd2c6ece0d2
Ankur Sao <ankur.sao7@gmail.com>
2020-06-23 10:13:37 +0530

    AVL Tree insert

---
+++ GeeksForGeeks/advanced-data-structure/AVL-tree.cpp
@@ -0,0 +1,256 @@
+
+/*  CKMKC
+ *  AVL Tree: Self balancing binary search tree
+ *  It's a regular simple BST with augmented methods (insertion and deletion)
+ *  and augmented nodes. (with height)
+ *
+ *  Insertion:
+ *   Let's assume we are inserting W. On ancestral path of W,
+ *   Z got imbalanced. 
+ *   Y is child of Z on the path to W.
+ *   X is  grandchild of Z on the path to W.
+ *
+ *             Z                                           Y
+ *           /   \       				 /   \
+ *         Y      T1      Right Rotate 		        X     Z
+ *        / \             ============>>>              / \   /  \
+ *       X   T2					      T3 T4  T2  T1
+ *      / \
+ *     T3 T4 
+ *
+ *   Case 1. LEFT/LEFT : Right Rotate, child(Y) becomes local root.
+ *   Y and X are  both left child.
+ *
+ *   T3 > X >T4 > Y >T2 > Z > T1   ==>  T3 > X > T4 > Y > T2 > Z > T1
+ *
+ *   
+ *   Case 2. LEFT/RIGHT: X becomes local root.
+ *   Y is left child. X is right child. 
+ *
+ *      Z						Z   				      X
+ *    /  \					      /   \	                            /   \    
+ *   Y   T1              LEFT rotate                 X     T1				   Y     Z	
+ *  / \                  ============>             /  \		    RIGHT Rotate          /\ 	/  \
+ * T2  X 					  Y    T4            ===========>        T2 T3  T4  T1
+ *    / \					 / \
+ *   T3  T4 					T2  T3
+ *
+ * T2 > Y > T3 > X >  T4 > Z > T1        ====>   T2 > Y > T3 > X > T4 > Z > T1
+ *
+ *
+ *   Case 3: RIGHT RIGHT: LEFT ROTATE: Y becomes local root.          
+ *  
+ *     Z		     LEFT Rotate              Y	
+ *   /   \                   ============>          /   \
+ *  T1    Y 					   Z 	 X
+ *       / \					 /  \	/ \
+ *      T2  X					T1  T2	T3 T4
+ *         / \
+ *        T3  T4  
+ *
+ * T1 > Z > T2 > Y > T3 > X > T4          ======>      T1 > Z > T2 > Y > T3 > X > T4
+ *
+ *
+ *   Case 4: RIGHT LEFT: X becomes local root.
+ *    
+ *     Z 					Z 						X
+ *   /  \          Right Rotation              /  \      Left Rotation            	      /   \	
+ *  T1   Y         ==============>	      T1   X	 ==================>                 Z     Y
+ *      / \     				  / \					    / \   / \
+ *     X   T2					 T3  Y					  T1  T3 T4  T2	 	
+ *    / \  					    / \	
+ *   T3 T4  					   T4  T2	
+ *
+ *
+ * T1 > Z > T3 > X > T4 > Y  > T2         ===================>     T1 >  Z > T3 > X > T4 > Y > T2
+ *
+ *
+ *
+ *
+ * */
+
+
+
+
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<utility>
+#include<string>
+#include<map>
+#include<fstream>
+#include<sstream>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector< vi > vvi;
+typedef vector< vvi > vvvi;
+typedef pair<ll,ll> ii;
+typedef vector<ii> vii;
+typedef set<ll> si;
+typedef map<string,ll> msi;
+
+
+    #define INF 20000000
+    #define pb push_back
+    #define mp make_pair
+    #define ff first
+    #define ss second
+    #define sz size()
+    #define ln length()
+    #define rep(i,n) for(ll i=0;i<n;i++)
+    #define fu(i,a,n) for(ll i=a;i<=n;i++)
+    #define fd(i,n,a) for(ll i=n;i>=a;i--)
+    #define all(a)  a.begin(),a.end()
+    #define pl(a) cout<<a
+    #define nl cout<<endl
+    #define present(container,element) container.find(element)!=container.end()
+    #define cpresent(container,element) find(all(container),element)!=container.end()
+
+
+ ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+class node
+{
+	public:
+	int key;
+	int height;
+	node *left, *right;
+
+	node(int val)
+	{
+		key =val;
+		height =0;
+		left=right=NULL;
+	}
+
+};
+
+	int  getHeight(node *A)
+	{
+		if (A==NULL) return 0;
+		else return A->height;
+	}
+
+       
+        int getBalance(node *A)
+	{
+		if (A == NULL) return 0;
+		else return getHeight(A->left) - getHeight(A->right);
+	}
+
+	node* leftRotate(node *Y)
+	{
+		/*
+		 *          Z				Z
+		 *         / \ 			       / \
+		 *        Y   T1		      T1  Y	
+		 *       / \             or              / \
+		 *	T2  X 			        T2  X
+		 *	   / \				   / \	
+		 *	  T3  T4			  T3  T4
+		 * */
+
+		node *X = Y->right;
+		node *T3 = X->left;
+
+		X->left = Y;
+		Y->right = T3;
+
+		Y->height =  max(getHeight(Y->left), getHeight(Y->right)) +1 ;
+                X->height  = max(getHeight(X->left), getHeight(X->right)) +1 ;
+		cout << "node: "<<X->key << " ;s height is : "<< X->height<<"\n";
+		return X;
+	}
+
+	node* rightRotate(node *Y)
+	{
+		/*
+		 *        Z 				   Z
+		 *       / \				 /   \
+		 *      T1  Y                 or        Y     T1
+		 *         / \                         / \ 
+		 *        X   T2 		      X   T2
+		 *       / \                         / \
+		 *      T3 T4                       T3  T4
+		 *
+		 * */
+		node  *X = Y->left;
+		node *T4 = X->right;
+
+		X->right = Y;
+		Y->left = T4;
+
+		Y->height =  max(getHeight(Y->left), getHeight(Y->right)) +1 ;
+                X->height  = max(getHeight(X->left), getHeight(X->right)) +1 ;
+		cout << "node: "<<Y->key << " ;s height is : "<< Y->height<<"\n";
+		return X;
+	}
+
+
+	node* insert(node *A,int key)
+	{
+
+		if  (A==NULL) return  new  node(key);
+		else if (key < A->key)
+		{
+			A->left = insert(A->left, key);
+		}
+		else
+		{
+			A->right = insert(A->right, key);
+		}
+	
+	        A->height = 1 + max (getHeight(A->left), getHeight(A->right));
+	        cout << "node: "<<A->key << " ;s height is : "<< A->height<<"\n";	
+		int balance = getBalance(A);
+ 
+		//left left
+		if (balance > 1 && key < A->left->key )
+		{
+			return  rightRotate(A);
+		}
+		//left right
+		else if (balance > 1 && key > A->left->key)
+		{
+			A->left  = leftRotate(A->left);
+			return rightRotate(A);
+		}
+		//right left
+		else if (balance < -1 && key < A->right->key)
+		{
+			A->right = rightRotate(A->right);
+			return leftRotate(A);
+		}
+		//right right
+		else if (balance < -1 && key > A->right->key)
+		{
+			return leftRotate(A);
+		}
+
+		return A;
+	}
+		
+
+int main()
+{
+	node *root = new node(15);
+	root = insert(root,10);
+	root = insert(root,20);
+	root = insert(root,30);
+	root = insert(root,40);
+	root = insert(root,50); 
+
+
+	cout<< "root's height: " << root->height<<"\n";
+	return 0;
+}
+
